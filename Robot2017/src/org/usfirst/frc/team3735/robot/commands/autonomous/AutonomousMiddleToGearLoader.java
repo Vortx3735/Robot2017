d3ae8369cee4e6ed63600fee6df970a4c896fc13
JMart127 <jtm5317@gmail.com>
2017-02-18 15:05:32 -0600

    Added more constatns and started autonomous

--- Robot2017/src/org/usfirst/frc/team3735/robot/commands/autonomous/AutonomousMiddleToGearLoader.java
+++ Robot2017/src/org/usfirst/frc/team3735/robot/commands/autonomous/AutonomousMiddleToGearLoader.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team3735.robot.commands.autonomous;
 
+import org.usfirst.frc.team3735.robot.Constants;
+import org.usfirst.frc.team3735.robot.commands.drive.DriveMoveDistance;
 import org.usfirst.frc.team3735.robot.commands.drive.DriveTurnToAngle;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -7,12 +9,27 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 /**
  *
  */
-public class AutonomousTest extends CommandGroup {
-
-    public AutonomousTest() {
-    	addSequential(new DriveTurnToAngle(90));
-    	addSequential(new DriveTurnToAngle(180));
-    	addSequential(new DriveTurnToAngle(0));
+// Start for the middle then drive to gear lift and place gear.
+// Then back up and and drive to gear loader
+public class AutonomousMiddleToGearLoader extends CommandGroup {
+	
+    public AutonomousMiddleToGearLoader() {
+    	//Drive To gear loader
+    	addSequential(new DriveMoveDistance(114.775-Constants.RobotDimensions.length));
+    	
+    	//addSequential(new put get drop off class)
+    	addSequential(new DriveMoveDistance(-65));
+    	addSequential(new DriveTurnToAngle(-60));
+    	addSequential(new DriveMoveDistance(173));
+    	addSequential(new DriveTurnToAngle(60));
+    	addSequential(new DriveMoveDistance(464));
+    	
+    	//finish at 52 in away from other team wall and 12 inches down from top wall
+    	
+    	
+    	
+    	
+    	
         // Add Commands here:
         // e.g. addSequential(new Command1());
         //      addSequential(new Command2());
