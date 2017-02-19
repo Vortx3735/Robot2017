e72a739633632fe85f74335426381effa9fa2d5f
JMart127 <jtm5317@gmail.com>
2017-02-19 11:04:02 -0600

    Added all Autonomous Classes

--- Robot2017/src/org/usfirst/frc/team3735/robot/commands/autonomous/AutonomousMiddleGearTopLoader.java
+++ Robot2017/src/org/usfirst/frc/team3735/robot/commands/autonomous/AutonomousMiddleGearTopLoader.java
@@ -1,26 +1,29 @@
 package org.usfirst.frc.team3735.robot.commands.autonomous;
 
-import org.usfirst.frc.team3735.robot.Constants;
-import org.usfirst.frc.team3735.robot.Constants.PointsLeft;
+//import org.usfirst.frc.team3735.robot.Constants;
+import org.usfirst.frc.team3735.robot.util.Point;
 import org.usfirst.frc.team3735.robot.commands.drive.DriveMoveDistance;
 import org.usfirst.frc.team3735.robot.commands.drive.DriveTurnToAngle;
-import org.usfirst.frc.team3735.robot.util.Point;
+import org.usfirst.frc.team3735.robot.Constants.PointsLeft;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
  *
  */
-public class AutonomousMiddleToBoiler extends CommandGroup {
+// Start for the middle then drive to gear lift and place gear.
+// Then back up and and drive to gear loader
+public class AutonomousMiddleGearTopLoader extends CommandGroup {
 	
-    public AutonomousMiddleToBoiler() {
+    public AutonomousMiddleGearTopLoader() {
     	Point point0 = PointsLeft.startMiddle;
     	Point point1 = PointsLeft.middleLift;
     	Point point2 = PointsLeft.midBackUp;
-    	Point point3 = PointsLeft.lineUpWithBoiler;
-    	Point point4 = PointsLeft.boiler;
+    	Point point3 = PointsLeft.retrivalAndBaseLine;
+    	Point point4 = PointsLeft.opSideRetrivalAndBaseLine;
     	
     	addSequential(new DriveTurnToAngle(Point.findAngle(point0, point1)));
     	addSequential(new DriveMoveDistance(Point.findDistance(point0, point1)));
+    	//addSequential(new GearDropOff());
     	addSequential(new DriveTurnToAngle(Point.findAngle(point1, point2)));
     	addSequential(new DriveMoveDistance(Point.findDistance(point1, point2)));
     	addSequential(new DriveTurnToAngle(Point.findAngle(point2, point3)));
