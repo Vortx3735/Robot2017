1ea568f26e45c3cacf5ebfafc15ec487c2a08a4f
AndrewNolte <anolte512@gmail.com>
2017-04-14 11:43:15 -0500

    finished PIDctrl class

--- Robot2017/src/org/usfirst/frc/team3735/robot/util/PIDCtrl.java
+++ Robot2017/src/org/usfirst/frc/team3735/robot/util/PIDCtrl.java
@@ -6,18 +6,73 @@ import edu.wpi.first.wpilibj.PIDSource;
 
 public class PIDCtrl extends PIDController{
 
+	private double iZone = 0;
+	private double actingI;
+	private boolean isUsingIZone = false;
+	
 	public PIDCtrl(double Kp, double Ki, double Kd, PIDSource source, PIDOutput output, double period) {
-		super(Kp, Ki, Kd, source, output, period);
+		super(Kp, 0, Kd, source, output, period);
+		actingI = Ki;
 	}
 	public PIDCtrl(double Kp, double Ki, double Kd, PIDSource source, PIDOutput output) {
-		super(Kp, Ki, Kd, source, output);
+		super(Kp, 0, Kd, source, output);
+		actingI = Ki;
 	}
 	public PIDCtrl(double Kp, double Ki, double Kd, double Kf, PIDSource source, PIDOutput output, double period) {
-		super(Kp, Ki, Kd, Kf, source, output, period);
+		super(Kp, 0, Kd, Kf, source, output, period);		
+		actingI = Ki;
 	}
 	public PIDCtrl(double Kp, double Ki, double Kd, double Kf, PIDSource source, PIDOutput output) {
-		super(Kp, Ki, Kd, Kf, source, output);
+		super(Kp, 0, Kd, Kf, source, output);
+		actingI = Ki;
 	}
+	
+	/*
+	 * these methods set and update the I-zone for the controller
+
+	 */
+	
+	public synchronized void setIZone(double izone){
+		iZone = izone;
+	}
+	
+	public synchronized void setIsUsingIZone(boolean b){
+		isUsingIZone = b;
+	}
+	
+	public synchronized void updateI(){
+		if(isUsingIZone){
+			if(Math.abs(super.getError()) < iZone){
+				super.setPID(super.getP(), actingI, super.getD());
+			}else{
+				super.setPID(super.getP(), 0, super.getD());
+			}
+		}else{
+			super.setPID(super.getP(), actingI, super.getD());
+		}
+		
+	}
+	
+	
+	/*
+	 * these methods route to the controller, manipulating instead the acting I
+	 */
+	
+	public synchronized void setPID(double p, double i, double d) {
+		actingI = i;
+		super.setPID(p, 0, d);	
+	}
+
+	
+	public synchronized void setPID(double p, double i, double d, double f) {
+		actingI = i;
+		super.setPID(p, 0, d, f);
+	}
+
+
+
+	
+	
 	
 	
 
